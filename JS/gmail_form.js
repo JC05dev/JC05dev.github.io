ef2c04f3da62d443626ed853e6a6cdd77ecbb040
jonascool05 <jonascoollolo@gmail.com>
2023-10-06 23:40:54 +0200

    added formspree to contact me via email

---
+++ JS/gmail_form.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.addEventListener('click', function (event) {
+      const gmailLogo = event.target.closest('.GMail-Logo');
+      if (gmailLogo) {
+        const gmailContact = event.target.closest('.GMail-Contact');
+        if (gmailContact) {
+          const gmailForm = gmailContact.querySelector('.GMail-Form');
+          if (gmailForm) {
+            // Toggle the visibility of the form
+            if (gmailForm.style.zIndex === '1') {
+              // If the form is visible, hide it
+              gmailForm.style.zIndex = '-1';
+              gmailForm.style.display = 'none';
+            } else {
+              // If the form is hidden, show it
+              gmailForm.style.zIndex = '1';
+              gmailForm.style.display = 'block';
+            }
+          } else {
+            console.error('GMail-Form not found');
+          }
+        } else {
+          console.error('GMail-Contact not found');
+        }
+      }
+      // The rest of your code
+    });
+  });
+  
