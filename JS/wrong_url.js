5397a3bf15fe53031a60822b29df36e92c26c719
jonascool05 <jonascoollolo@gmail.com>
2023-10-10 23:25:51 +0200

    fixed a typo and edited a comment

--- JS/wrong_url.js
+++ JS/wrong_url.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the incorrect URL from the referrer
+    // Get the current URL from the window's location
     var incorrectUrl = window.location.href;
 
     // Display the incorrect URL
     var errorMessage = document.getElementById("errorMessage");
-    errorMessage.textContent = "Diese URL existtiert nicht: " + incorrectUrl;
+    errorMessage.textContent = "Diese URL existiert nicht: " + incorrectUrl;
 });
