b76120849c84e2bd812561d9cf62e161942a918a
jonascool05 <jonascoollolo@gmail.com>
2023-10-12 21:41:17 +0200

    added translation to the E-Mail Form

--- JS/translations.js
+++ JS/translations.js
@@ -4,11 +4,17 @@ const translations = {
         'about_me': 'About Me',
         'resume': 'Resume/Report Card(s)',
         'contact': 'Contact',
+        'subject' : 'Subject',
+        'message' : 'Message',
+        'submit' : 'submit',
     },
     'de': {
         'home': 'Home',
         'about_me': 'Über Mich',
         'resume': 'Lebenslauf/Zeugnis',
         'contact': 'Kontakt',
+        'subject' : 'Betreff',
+        'message' : 'Nachricht',
+        'submit' : 'Senden'
     }
 };
