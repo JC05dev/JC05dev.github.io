9b247cf312fcfe3004e6f42e1b3c6d2833651524
jonascool05 <jonascoollolo@gmail.com>
2023-10-12 20:39:32 +0200

    moved translation keys seperate javascript file

--- JS/translate.js
+++ JS/translate.js
@@ -1,18 +1,3 @@
-const translations = {
-    'en': {
-        'home': 'Home',
-        'about_me': 'About Me',
-        'resume': 'Resume/Report Card(s)',
-        'contact': 'Contact',
-    },
-    'de': {
-        'home': 'Home',
-        'about_me': 'Über Mich',
-        'resume': 'Lebenslauf/Zeugnis',
-        'contact': 'Kontakt',
-    }
-};
-
 // Function to change language
 function changeLanguage(language) {
     localStorage.setItem('selectedLanguage', language);
---
+++ JS/translations.js
@@ -0,0 +1,14 @@
+const translations = {
+    'en': {
+        'home': 'Home',
+        'about_me': 'About Me',
+        'resume': 'Resume/Report Card(s)',
+        'contact': 'Contact',
+    },
+    'de': {
+        'home': 'Home',
+        'about_me': 'Über Mich',
+        'resume': 'Lebenslauf/Zeugnis',
+        'contact': 'Kontakt',
+    }
+};
