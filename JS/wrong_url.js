25cee1cc064f789a2fc43ab3a143f236ded5615e
jonascool05 <jonascoollolo@gmail.com>
2023-10-14 01:14:18 +0200

    removed wrong_url, translate.js now handles it

--- JS/wrong_url.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the current URL from the window's location
-    var incorrectUrl = window.location.href;
-
-    // Display the incorrect URL
-    var errorMessage = document.getElementById("errorMessage");
-    errorMessage.textContent = "Diese URL existiert nicht: " + incorrectUrl;
-});
