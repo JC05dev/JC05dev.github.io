2edd139943afa6f09ac9d93abd58fc371f157e1c
jonascool05 <jonascoollolo@gmail.com>
2023-10-18 08:43:34 +0200

    Portrait Mode has been added back

--- JS/orientation.js
+++ JS/orientation.js
@@ -1,15 +1,19 @@
 // Function to handle orientation changes
 function handleOrientationChange() {
+    const body = document.body;
+    const contentDiv = document.querySelector('.Content');
+
     if (window.matchMedia("(orientation: portrait)").matches) {
-      document.body.classList.add('portrait-mode');
+        contentDiv.classList.remove('landscape');
+        contentDiv.classList.add('portrait');
     } else {
-      document.body.classList.remove('portrait-mode');
+        contentDiv.classList.remove('portrait');
+        contentDiv.classList.add('landscape');
     }
-  }
-  
-  // Initial check on page load
-  handleOrientationChange();
-  
-  // Listen for changes in orientation
-  window.addEventListener('resize', handleOrientationChange);
-  
+}
+
+// Initial check on page load
+handleOrientationChange();
+
+// Listen for changes in orientation
+window.addEventListener('resize', handleOrientationChange);
