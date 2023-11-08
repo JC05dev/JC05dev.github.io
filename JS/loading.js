993786b47cbc500daab8a7f303d945d1775fd919
jonascool05 <jonascoollolo@gmail.com>
2023-11-08 09:00:00 +0100

    added a loading screen

---
+++ JS/loading.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Show loading overlay on page load
+    var loadingOverlay = document.getElementById("loadingOverlay");
+    var loadingIcon = document.getElementById("loadingIcon");
+    loadingOverlay.style.display = "flex";
+
+    // Hide loading overlay after a delay
+    setTimeout(function() {
+        loadingOverlay.style.display = "none";
+        loadingIcon.style.display = "none";
+    }, 15000000);
+});
