a3b5080683473c25d913d07bcd5c3a05a3af87dd
jonascool05 <jonascoollolo@gmail.com>
2023-11-08 19:42:06 +0100

    added a "legal" page

--- JS/translations.js
+++ JS/translations.js
@@ -1,28 +1,36 @@
 const translations = {
     'en': {
+        //Navigation Translations
         'home': 'Home',
         'about_me': 'About Me',
         'resume': 'Resume/Report Card(s)',
         'contact': 'Contact',
+        'legal' : 'Legal',
+        //Gmail-Form Translations
         'subject' : 'Subject',
         'message' : 'Message',
         'submit' : 'submit',
         'clear' : 'clear',
         'cancel' : 'cancel',
+        //404.html specific translation
         'errorMessage' : 'This URL doesnt exist ',
         'try_these' : 'Try these URLs',
 
     },
     'de': {
+        //Navigation Translations
         'home': 'Home',
         'about_me': 'Über Mich',
         'resume': 'Lebenslauf/Zeugnis',
         'contact': 'Kontakt',
+        'legal' : 'Rechtliches',
+        //Gmail-Form Translations
         'subject' : 'Betreff',
         'message' : 'Nachricht',
         'submit' : 'Senden',
         'clear' : 'Löschen', 
         'cancel' : 'Abbrechen',
+        //404.html specific translation
         'errorMessage' : 'Diese URL existiert nicht ',
         'try_these' : 'Probieren sie es mit Diesen URLs',
     }
