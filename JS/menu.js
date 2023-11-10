087716a1323e5d0dcfde94b43b0ebffcd4114a3a
jonascool05 <jonascoollolo@gmail.com>
2023-11-10 21:42:04 +0100

    made the title responsive and fixed clipping

--- JS/menu.js
+++ JS/menu.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navIcon = document.querySelector('.nav-icon');
   const navLinks = document.querySelectorAll('.nav-link');
+  const title = document.querySelector('.Title');
 
   navIcon.addEventListener('click', function () {
     const topnav = document.querySelector('.topnav');
     topnav.classList.toggle('responsive');
+    title.classList.toggle('responsive');
   });
 
   // Hide the menu when a link is clicked
@@ -13,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const topnav = document.querySelector('.topnav');
       if (topnav.classList.contains('responsive')) {
         topnav.classList.remove('responsive');
+        title.classList.remove('responsive');
       }
     });
   });
